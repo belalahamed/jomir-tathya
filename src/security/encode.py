6108ad21d059f3eaa21c97a0686afd67db64d8d3
Belal Ahamed <belalahamed.dev@gmail.com>
2026-04-17 17:33:46 +0530

    change: made some minor chnages

--- src/security/encode.py
+++ src/security/encode.py
@@ -7,13 +7,13 @@ import base64
 
 def encode_user_type(user_type: str) -> str:
     """Encoding user type in base64 format with a special string"""
-    
+
     encoded_user_type = "RGxycyMxMjM=" + base64.b64encode(user_type.encode()).decode()
     return encoded_user_type
 
 
 def encode_user_id(user_id: str) -> str:
     """Encoding user id in base64 format with a special string"""
-    
+
     encoded_user_id = "RGxycyMxMjM=" + base64.b64encode(user_id.encode()).decode()
     return encoded_user_id
--- src/security/encrypt.py
+++ src/security/encrypt.py
@@ -1,30 +1,37 @@
 import hashlib
 
+
 def md5_hex(data: str) -> str:
-    return hashlib.md5(data.encode('utf-8')).hexdigest()
+    return hashlib.md5(data.encode("utf-8")).hexdigest()
+
 
 def sha256_hex(data: str) -> str:
-    return hashlib.sha256(data.encode('utf-8')).hexdigest()
+    return hashlib.sha256(data.encode("utf-8")).hexdigest()
+
 
 def md5_with_key(key: str, data: str) -> str:
     return md5_hex(data + key)
 
+
 def encrypt_password(password: str, salt: str) -> str:
+    """Encrypt Password using Hashing"""
+
     if not password:
         return ""
-    
+
     md5_encrypted = md5_hex(password)
     salted_md5 = md5_with_key(salt, md5_encrypted)
     encrypted_password = sha256_hex(salted_md5)
-
     return encrypted_password
 
+
 def encrypt_user_id(username: str, salt: str) -> str:
+    """Encrypt Username using Hashing"""
+
     if not username:
         return ""
-    
+
     md5_encrypted = md5_hex(username)
     salted_md5 = md5_with_key(salt, md5_encrypted)
     encrypted_user_id = salted_md5
-
     return encrypted_user_id
