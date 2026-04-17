91c704b55d30f251a04c6f9473d97fe6a1bec853
Belal Ahamed <belalahamed.dev@gmail.com>
2026-04-17 21:23:16 +0530

    fix: images path issue fixed

--- assets/__init__.py
+++ assets/__init__.py
@@ -1,6 +1,6 @@
 from pathlib import Path
 
-app_icon = Path(__file__).parent / "assets" / "images" / "app_icon.ico"
-app_logo = Path(__file__).parent / "assets" / "images" / "app_logo.png"
+app_icon = Path(__file__).parent / "images" / "app_icon.ico"
+app_logo = Path(__file__).parent / "images" / "app_logo.png"
 
 __all__ = ["app_icon", "app_logo"]
