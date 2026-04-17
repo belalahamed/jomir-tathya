a57d9c6814452057bf5724decec3bb6276b1d0c3
Belal Ahamed <belalahamed.dev@gmail.com>
2026-04-17 23:29:34 +0530

    add: added new icon

--- assets/__init__.py
+++ assets/__init__.py
@@ -2,5 +2,6 @@ from pathlib import Path
 
 app_icon = Path(__file__).parent / "images" / "app_icon.ico"
 app_logo = Path(__file__).parent / "images" / "app_logo.png"
+back_icon = Path(__file__).parent / "images" / "left_arrow.png"
 
-__all__ = ["app_icon", "app_logo"]
+__all__ = ["app_icon", "app_logo", "left_arrow"]
