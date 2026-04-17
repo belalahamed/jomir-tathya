3bd14a58f64d6fc5e226e4091c1cd6e869ec2760
Belal Ahamed <belalahamed.dev@gmail.com>
2026-04-17 17:30:28 +0530

    refactor: made some refactor to portal

--- src/api/__init__.py
+++ src/api/__init__.py
@@ -1,4 +1,3 @@
 from .portal import fetch_districts, fetch_blocks, fetch_mouzas, fetch_khatian, fetch_plot
-from .api_urls import API_URLs as api_urls
 
-__all__ = ["fetch_districts", "fetch_blocks", "fetch_mouzas", "fetch_khatian", "fetch_plot", "api_urls"]
+__all__ = ["fetch_districts", "fetch_blocks", "fetch_mouzas", "fetch_khatian", "fetch_plot"]
